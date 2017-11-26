cdac2b540366f9f5030d8a59ba0ff482a1a83c5c
whitefire0 <rickhallett@live.co.uk>
2017-11-26 17:28:14 +0000

    initial commit

---
+++ myToFixed.js
@@ -0,0 +1,39 @@
+/*!
+ * "The Beasts of Watch And Code"
+ * A twist on accounting.toFixed v0.1.0
+ * Copyright 2017 whitefires0 / Rick Hallett
+ *
+ * Freely distributable under the creative commons license.
+ *
+ * Full details and documentation:
+ * https://github.com/gordonmzhu/beasts/issues/7
+ */
+//=============================================================================================================
+    /* --- Project Implementation --- */
+//=============================================================================================================
+/*!
+ * "The Beasts of Watch And Code"
+ * A twist on accounting.toFixed v0.1.0
+ * Copyright 2017 whitefire0 / Rick Hallett
+ *
+ * Freely distributable under the creative commons license.
+ *
+ * Full details and documentation:
+ * http://github.com/gordonmzhu/beasts/issues/7
+ */
+
+/*
+Process:
+
+
+ */
+  (function main(root) {
+      
+    function myToFixed() {
+
+    }
+
+      //export library for window access (where root = window object)
+      root.myToFixed = myToFixed;
+
+  })(this);
